7a02d4fe8984ceb83ab4540d1f60a35db98bbe40
j2427198 <j2427198@gmail.com>
2025-07-20 11:46:35 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+# The same profile name we used it in the ~/.aws/config file
+aws-export-credentials --profile dev
+# you should see somthing like this in reposne
+{
+  "Version": 1,
+  "AccessKeyId": "ASXXXXXXXXXXXXXEQOZ",
+  "SecretAccessKey": "YvIxxxDv593xxx ...",
+  "SessionToken": "IQoJb3JpZ2luX2VjE ...",
+  "Expiration": "2023-07-28T10:34:03Z"
+}
