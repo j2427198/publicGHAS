f16ff4f835882e12c43da83628ae068cd85a1093
j2427198 <j2427198@gmail.com>
2025-07-20 12:02:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,9 +1,12 @@
 # The same profile name we used it in the ~/.aws/config file
 aws-export-credentials --profile dev
 # you should see somthing like this in reposne
+
+
+aws_secret_access_key
 {
   "Version": 1,
-  "AccessKeyId": "ASHJHJHJHKGYUIOEQOZ",
-  "SecretAccessKey": "YvIKLPODv593PKL",
+  "aws_access_key_id": "ASHJHJHJHKGYUIOEQOZ",
+  "aws_secret_access_key": "YvIKLPODv593PKL",
 
 }
